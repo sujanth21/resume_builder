1aba8783a86a31661dda6f9272e69c1db185277c
Sujanth <sujanth1987@gmail.com>
2022-04-07 16:50:59 +1000

    card animation added

--- src/components/WhyUs/WhyUs.jsx
+++ src/components/WhyUs/WhyUs.jsx
@@ -4,8 +4,9 @@ import FreeImg from "../../assets/free.png";
 import CreativeImg from "../../assets/creative.png";
 import HiddenFeeImg from "../../assets/hidden_fee.png";
 import SearchImg from "../../assets/search.png";
-import FilterImg from "../../assets/glasses.png";
 import CustomizeImg from "../../assets/customize.png";
+
+import { motion } from "framer-motion";
 
 import styles from "./WhyUs.module.css";
 
@@ -15,40 +16,75 @@ const WhyUs = () => {
       <h3 className={styles.heading}>Why ReBuild?</h3>
 
       <div className={styles.box_container}>
-        <div className={styles.box}>
+        <motion.div
+          className={styles.box}
+          whileHover={{
+            scale: 1.1,
+            textShadow: "0px 0px 8px rgba(255, 255,255)",
+            boxShadow: "0px 0px 8px rgba(#32936f,#32936f,#32936f)",
+          }}
+        >
           <img src={FreeImg} alt='free' />
           <h3>FREE</h3>
           <p>We offer only FREE services to all users around the globe.</p>
-        </div>
+        </motion.div>
 
-        <div className={styles.box}>
+        <motion.div
+          className={styles.box}
+          whileHover={{
+            scale: 1.1,
+            textShadow: "0px 0px 8px rgba(255, 255,255)",
+            boxShadow: "0px 0px 8px rgba(#32936f,#32936f,#32936f)",
+          }}
+        >
           <img src={CreativeImg} alt='free' />
           <h3>Be Creative</h3>
           <p>Our template is designed to satisfy employer expectations</p>
-        </div>
+        </motion.div>
 
-        <div className={styles.box}>
+        <motion.div
+          className={styles.box}
+          whileHover={{
+            scale: 1.1,
+            textShadow: "0px 0px 8px rgba(255, 255,255)",
+            boxShadow: "0px 0px 8px rgba(#32936f,#32936f,#32936f)",
+          }}
+        >
           <img src={SearchImg} alt='free' />
           <h3>ATS Friendly</h3>
           <p>Built with Applicant Tracking Systems in mind</p>
-        </div>
+        </motion.div>
 
-        <div className={styles.box}>
+        <motion.div
+          className={styles.box}
+          whileHover={{
+            scale: 1.1,
+            textShadow: "0px 0px 8px rgba(255, 255,255)",
+            boxShadow: "0px 0px 8px rgba(#32936f,#32936f,#32936f)",
+          }}
+        >
           <img src={HiddenFeeImg} alt='free' />
           <h3>No Hideen Fees</h3>
           <p>
             All the features in our platform are always FREE, no hidden cost
             ever.
           </p>
-        </div>
+        </motion.div>
 
-        <div className={styles.box}>
+        <motion.div
+          className={styles.box}
+          whileHover={{
+            scale: 1.1,
+            textShadow: "0px 0px 8px rgba(255, 255,255)",
+            boxShadow: "0px 0px 8px rgba(#32936f,#32936f,#32936f)",
+          }}
+        >
           <img src={CustomizeImg} alt='free' />
           <h3>Customizable</h3>
           <p>
             Friendly customization and easy to organize by using drag and drop
           </p>
-        </div>
+        </motion.div>
       </div>
     </section>
   );
